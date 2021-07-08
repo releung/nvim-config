68e417d753064cde14db4dfe32d4db12fc21e5d5
ElliotLeung <LZ@sz-easyway.com>
2021-07-08 16:27:44 +0800

    修改 docs/Nvim_setup.sh git clone 地址为本仓库

--- docs/Nvim_setup.sh
+++ docs/Nvim_setup.sh
@@ -175,7 +175,7 @@ if [[ -d "$NVIM_CONFIG_DIR" ]]; then
     mv "$NVIM_CONFIG_DIR" "$NVIM_CONFIG_DIR.backup"
 fi
 
-git clone --depth=1 https://hub.fastgit.org//jdhao/nvim-config.git "$NVIM_CONFIG_DIR"
+git clone --depth=1 https:/hub.fastgit.org/releung/nvim-config.git -b dev-leung "$NVIM_CONFIG_DIR"
 
 echo "Installing packer.nvim"
 git clone --depth=1 https://hub.fastgit.org//wbthomason/packer.nvim \
